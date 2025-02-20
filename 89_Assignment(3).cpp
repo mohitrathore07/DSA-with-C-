4d8f02ced191d978b2e60139f7a65a06fc85c1b3
Mohit Rathore <mohitrathore8269@gmail.com>
2025-02-20 21:33:08 +0530

    assignment recursion

--- 89_Assignment(3).cpp
+++ 89_Assignment(3).cpp
@@ -53,3 +53,148 @@ int main () {
 }
 
 */
+
+/* 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printa(vector<int>& arr1, vector<int>& v , vector<vector<int>>& ans  ,  int idx , int n){
+    if(idx == n) {
+        ans.push_back(v);    
+        return;
+    }
+    printa(arr1, v , ans ,idx+1,n);
+    v.push_back(arr1[idx]);
+    printa(arr1, v , ans, idx+1,n);
+}
+
+int main () {
+    vector<int> arr1= {1 , 2 ,3};
+    vector<int> v;
+    vector<vector<int>> ans;
+    printa(arr1 , v , ans, 0 , 3);
+    int row = ans.size();
+    int column = ans[0].size();
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j  < column; j++)
+        {
+          cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
+*/
+
+//// Assignment - 2
+
+/*
+// leet code  78
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printa(vector<int>& arr1, vector<int> v , vector<vector<int>>& ans ,  int idx , int n) {
+    if (idx == n) {
+        ans.push_back(v);    
+        return;
+    }
+
+    printa(arr1, v , ans, idx + 1, n);
+    v.push_back(arr1[idx]);
+    printa(arr1, v , ans, idx + 1, n);
+}
+
+int main() {
+    vector<int> arr1 = {1, 2, 2};
+    vector<int> v;
+    vector<vector<int>> ans;
+
+    printa(arr1, v, ans, 0, 3);
+
+    int row = ans.size();
+    int column = 0;
+    for (const auto& subset : ans) {
+       column = max(column, (int)subset.size()); 
+    }
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            if (j < ans[i].size()) {
+                cout << ans[i][j] << " ";
+            } else {
+                cout << "  "; 
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+*/
+
+/* 
+// leet code  90
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printa(vector<int>& arr1, vector<int> v , vector<vector<int>>& ans ,  int idx , int n) {
+    if (idx == n) {
+    int row = ans.size();
+    int column = 0;
+    for (const auto& subset : ans) {
+       column = max(column, (int)subset.size()); 
+    }
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            if (j < ans[i].size()) {
+                if(ans[i][j] != v[i]) {
+                    ans.push_back(v);
+                }
+                else {
+                    continue;
+                }
+            } 
+        }
+    } 
+        return;
+    }
+
+    printa(arr1, v , ans, idx + 1, n);
+    v.push_back(arr1[idx]);
+    printa(arr1, v , ans, idx + 1, n);
+}
+
+int main() {
+    vector<int> arr1 = {1, 2, 2};
+    vector<int> v;
+    vector<vector<int>> ans;
+
+    printa(arr1, v, ans, 0, 3);
+
+    int row = ans.size();
+    int column = 0;
+    for (const auto& subset : ans) {
+       column = max(column, (int)subset.size()); 
+    }
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            if (j < ans[i].size()) {
+                cout << ans[i][j] << " ";
+            } else {
+                cout << "  "; 
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+*/
+
